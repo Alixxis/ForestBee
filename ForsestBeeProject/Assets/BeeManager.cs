2b9b780a78e34ebea8b478754688f623d3869d00
Rudy <ruddycarpio@gmail.com>
2018-08-16 15:10:20 +1000

    Destroy red bee and instantiate blue

---
+++ ForsestBeeProject/Assets/BeeManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// The BeeManager manags the live cycle of bees
+/// </summary>
+public class BeeManager : MonoBehaviour
+{
+    public static Action<Bee> OnBeeHit;
+
+    public Bee m_BeeBluePrefab;
+
+    private void Awake()
+    {
+        //Subscribe BeeHit method to OnBeeHit event that gets called by Bee
+        OnBeeHit += BeeHit;
+    }
+
+    private void OnDestroy()
+    {
+        //Unsubscribe BeeHit method to OnBeeHit event that gets called by Bee
+        OnBeeHit -= BeeHit;
+    }
+
+    //When a bee gets hit by a projectile (mushroom?)
+    public void BeeHit(Bee bee) 
+    {
+        //Create a new bee
+        CreateBee(m_BeeBluePrefab,bee.transform.position, bee.transform.rotation);
+        //Destroy old one
+        DestroyBee(bee.gameObject);
+    }
+
+    //Generates a new bee at X location
+    public void CreateBee(Bee bee,Vector3 position, Quaternion rotation)
+    {
+        var beeInstance = Instantiate(bee, position, rotation);
+    }
+
+    //Destroy 
+    public void DestroyBee(GameObject go)
+    {
+        Destroy(go);
+    }
+
+}
---
+++ ForsestBeeProject/Assets/Forest_Project/Scripts/Bee.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Bee : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //Notify BeeManager that I've been hit by invoking the OnBeeHit event
+        if(BeeManager.OnBeeHit != null)
+            BeeManager.OnBeeHit.Invoke(this);
+    }
+}
