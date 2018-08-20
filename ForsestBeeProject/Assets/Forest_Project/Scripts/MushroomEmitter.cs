8955f152954bc0dc80e81739ed13a16e9e04d1eb
Rudy <ruddycarpio@gmail.com>
2018-08-20 13:12:46 +1000

    MushroomBullet added

---
+++ ForsestBeeProject/Assets/Forest_Project/Scripts/MushroomEmitter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MushroomEmitter : MonoBehaviour
+{
+    public GameObject Emitter;
+    public GameObject MushroomBullet;
+    public float MushroomBullet_Up_Force;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            GameObject Temporary_MushroomBullet_Handler;
+            Temporary_MushroomBullet_Handler = Instantiate(MushroomBullet, Emitter.transform.position, Emitter.transform.rotation) as GameObject;
+
+            Temporary_MushroomBullet_Handler.transform.Rotate(Vector3.forward * 0);
+
+            Rigidbody Temporary_RigidBody;
+            Temporary_RigidBody = Temporary_MushroomBullet_Handler.GetComponent<Rigidbody>();
+
+            Temporary_RigidBody.AddForce(transform.up * MushroomBullet_Up_Force);
+
+            Destroy(Temporary_MushroomBullet_Handler, 3.0f);
+        }
+    }
+}
