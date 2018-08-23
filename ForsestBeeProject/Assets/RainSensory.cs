a4c8a6e7605fac3972052dec1ab8b26e501cf736
Rudy <ruddycarpio@gmail.com>
2018-08-23 11:47:18 +1000

    Rain now set fire off

---
+++ ForsestBeeProject/Assets/RainSensory.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Cast a ray and sends 'Rain' to any possible smoke
+/// </summary>
+public class RainSensory : MonoBehaviour
+{
+    private RaycastHit _hit;
+    public LayerMask LayerMask;
+
+    private void Awake()
+    {
+        StartCoroutine(RainDetection());
+    }
+
+    IEnumerator RainDetection()
+    {
+        while (true)
+        {
+            //Update interval
+            yield return new WaitForSeconds(1);
+            TryRemoveSmoke();
+        }
+    }
+
+    public void TryRemoveSmoke()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out _hit, 100, LayerMask))
+        {
+            var smoke = _hit.transform.GetComponent<Smoke>();
+            if (smoke != null)
+            {
+                smoke.TakeHit(20);
+            }
+        }
+    }
+}
---
+++ ForsestBeeProject/Assets/Smoke.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smoke : MonoBehaviour
+{
+    public Transform SmokeContainer;
+    public float MaxHealth = 100;
+    public float Health = 100;
+
+    private void Awake()
+    {
+        _startScale = SmokeContainer.localScale;
+    }
+
+    public void TakeHit(float amount)
+    {
+        Health -= amount;
+
+        if (Health <= 0)
+            Health = 0;
+
+        ApplySmokeLevel();
+    }
+
+    public void ApplySmokeLevel()
+    {
+        var remainingPercentage = Health / MaxHealth;
+        SmokeContainer.localScale = Vector3.Lerp(Vector3.zero, _startScale, remainingPercentage);
+    }
+
+    private Vector3 _startScale;
+}
